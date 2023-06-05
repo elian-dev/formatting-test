ce13d1319eb7845cb37c60b64c9445a44a19428c
elian-dev <geordymemdoza@gmail.com>
2023-06-05 11:44:42 -0600

    Test format

--- public/assets/js/tabs.js
+++ public/assets/js/tabs.js
@@ -39,7 +39,7 @@ export function Tabs () {
   tabLinks.forEach(function (tab, i) {
     tab.addEventListener('click', (e) => {
       e.preventDefault()
-      const currentTabList = e.currentTarget.closest('ul[role="tablist"]')
+      const currentTabList = e.currentTarget.closest("ul[role="tablist"]")
       const currentTab = currentTabList.querySelector('[aria-selected]')
       if (e.currentTarget !== currentTab) {
         switchTab(currentTab, e.currentTarget)
